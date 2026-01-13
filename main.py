d74ef9286b68b209338e04eb5d44cb35e870a32e
Anderson <anddamere@gmail.com>
2026-01-12 21:43:12 -0300

    Ajuste na criacao automatica de tabelas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dinhoflix import app, database
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-with app.app_context():
-    database.create_all()
+    with app.app_context():
+        database.create_all()
+    app.run(debug=False)
